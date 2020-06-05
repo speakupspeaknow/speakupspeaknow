faf70e5aa583e8ab1eede23c351adc454740c595
Bibek <ghim.bibek@gmail.com>
2020-06-05 13:46:33 -0700

    almost ready for launch

--- src/data/email.ts
+++ src/data/email.ts
@@ -5,12 +5,12 @@ export interface BodyOptions {
   city: string
 }
 
-export const makeBody = (opts: BodyOptions) => `
-Hello, 
+export const makeBody = (opts: BodyOptions) =>
+  `Hello, 
 
 My name is ${opts.name || '[INSERT NAME HERE]'}. I am a resident of ${
-  opts.city
-} and I am emailing to demand the restructuring of our city budget, so as to prioritize more social services for our community, and to drastically minimize spending on Police. It is unconscionable that 1/3 to 1/2 of the city’s budget is going to the police department. 
+    opts.city
+  } and I am emailing to demand the restructuring of our city budget, so as to prioritize more social services for our community, and to drastically minimize spending on Police. It is unconscionable that 1/3 to 1/2 of the city’s budget is going to the police department. 
 
 This does not align with the values that I have as your constituent and I demand that you and other city officials work together to draft and approve a budget that diverts funds from the police department and reallocates them directly to benefit those in need. 
 
@@ -39,5 +39,4 @@ Provide cheaper and cleaner modes of public transportation
 Our nation is grieving the deaths of Black Americans that were murdered at the hands of police officers who have yet to be held accountable. While the police department has more funding than it knows what to do with, we have communities who desperately need funding and every day they don't receive it their quality of life worsens. Thousands have died who did not need to. You have the ability to change this, so do it. 
 
 Sincerely, 
-${opts.name || '[INSERT NAME HERE]'}
-`
+${opts.name || '[INSERT NAME HERE]'}`
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -6,7 +6,7 @@ import * as Form from 'src/components/Form'
 import { ExternalLink } from 'src/components/Link'
 import Navbar from 'src/components/Navbar'
 import * as Text from 'src/components/Text'
-import { cityToEmails } from 'src/data/cityData'
+import { cityToEmails, getEmailsForCity } from 'src/data/cityData'
 import * as emailData from 'src/data/email'
 import { colors, typography } from 'src/styles'
 import { makeMailToLink } from 'src/utils/mailto'
@@ -20,14 +20,6 @@ const cityOptions = Object.keys(cityToEmails).map((cityName) => ({
   value: cityName,
   label: cityName,
 }))
-
-const getEmailsForCity = (city: string) => {
-  return cityToEmails[city]
-}
-
-interface FormData {
-  name: string
-}
 
 const LandingPage = () => {
   const [selectedCity, setSelectedCity] = React.useState<CityOption | null>(
@@ -50,89 +42,129 @@ const LandingPage = () => {
   )
 
   return (
-    <Box minHeight="100vh" container overflowX="hidden" bg="black" py={4}>
+    <Box
+      minHeight="100vh"
+      width="100%"
+      overflowX="hidden"
+      bg="black"
+      py={4}
+      display="flex"
+      flexDirection="column"
+      alignItems="stretch"
+    >
       <Navbar />
       <Box
-        container={{ _: { maxWidth: '90%' }, md: { maxWidth: '70%' } }}
-        display="flex"
-        flexDirection="column"
+        container={{
+          _: { maxWidth: '90%' },
+          lg: { maxWidth: '50em' },
+        }}
       >
-        <Text.Heading color="white" fontWeight={700} mb={3}>
-          Let's hold our elected officials accountable.
-        </Text.Heading>
+        <Box
+          width="100%"
+          pt={{ _: 2, md: 2 }}
+          pb={{ _: 3, md: 4 }}
+          display="flex"
+          flexDirection="column"
+        >
+          <Text.Heading color="white" fontWeight={700} mb={3}>
+            Let's hold our elected officials accountable.
+          </Text.Heading>
 
-        <Text.SectionSubheader color="white">I live in</Text.SectionSubheader>
-        <Select
-          css={css(typography.textStyles.body)}
-          value={selectedCity}
-          onChange={handleCityChange}
-          options={cityOptions}
-        />
-
-        {selectedCity && (
-          <Box
-            mt={3}
-            display="flex"
-            flexDirection="column"
-            alignItems="stretch"
-          >
-            <Text.SectionSubheader color="white">
-              and my name is
-            </Text.SectionSubheader>
-            <Form.Input
-              py={2}
-              name="name"
-              value={personName}
-              onChange={handleNameChange}
-            />
-          </Box>
-        )}
-
-        {personName && selectedCity && (
-          <Box
-            mt={3}
-            display="flex"
-            flexDirection="column"
-            alignItems="stretch"
-          >
-            <Text.SectionSubheader color="white">
-              I've had enough. It's time to demand change.
-            </Text.SectionSubheader>
-            <Box mt={4}>
-              <ExternalLink
-                asButton
-                noUnderline
-                target="_blank"
-                buttonStyle="primary"
-                href={makeMailToLink({
-                  to: getEmailsForCity(selectedCity.value),
-                  subject: emailData.subject,
-                  body: emailData.makeBody({
-                    name: personName,
-                    city: selectedCity.label,
-                  }),
-                })}
-              >
-                Send email to {selectedCity.label} officials
-              </ExternalLink>
-            </Box>
-          </Box>
-        )}
-
-        <Box mt={5} pt={3} borderTop={`1px solid ${colors.textGray}`}>
-          <Text.Body>
-            Speak up, speak now is an intitative that empowers the people to
-            hold their city officials accountable. Thanks to the help of a LOT
-            of people, we have been able to collect the contact information of
-            city officials in many California cities. The fact that Los Angeles
-            has a Democratic Mayor with 14 of it's 15 council members being
-            Democrats and are following up a city budget that gave 52% of
-            allotment to the police and are considering a new one that cuts
-            social services while increasing police budget allotment to 57%
-            shows that voting is not enough. We MUST hold our democratically
-            elected representatives accountable and have our best interests at
-            heart.
+          <Text.Body color="white" mb={3}>
+            It is unacceptable that for many cities, 1/3 to 1/2 of the budget is
+            allotted to the police when communities are in need of social safety
+            nets, now more than ever. We MUST hold our democratically elected
+            representatives accountable and ensure that they have our best
+            interests at heart. One way we can do that is by sending them emails
+            demanding that they re-structure the budget: cutting funding for
+            police departments in favor of important civic measures.
           </Text.Body>
+
+          <Text.Body color="white" mb={3}>
+            All you need to do is select your city and enter your name below for
+            a pre-drafted email to send to your city’s officials.
+          </Text.Body>
+
+          <Text.SectionSubheader color="white">I live in</Text.SectionSubheader>
+          <Select
+            css={css(typography.textStyles.body)}
+            value={selectedCity}
+            onChange={handleCityChange}
+            options={cityOptions}
+          />
+
+          {selectedCity && (
+            <Box
+              mt={3}
+              display="flex"
+              flexDirection="column"
+              alignItems="stretch"
+            >
+              <Text.SectionSubheader color="white">
+                my name is
+              </Text.SectionSubheader>
+              <Form.Input
+                py={2}
+                name="name"
+                value={personName}
+                onChange={handleNameChange}
+              />
+            </Box>
+          )}
+
+          {personName && selectedCity && (
+            <Box
+              mt={3}
+              display="flex"
+              flexDirection="column"
+              alignItems="stretch"
+            >
+              <Text.SectionSubheader color="white">
+                and I've had enough. It's time to demand change.
+              </Text.SectionSubheader>
+              <Box mt={4}>
+                <ExternalLink
+                  asButton
+                  noUnderline
+                  target="_blank"
+                  buttonStyle="primary"
+                  href={makeMailToLink({
+                    to: getEmailsForCity(selectedCity.value),
+                    subject: emailData.subject,
+                    body: emailData.makeBody({
+                      name: personName,
+                      city: selectedCity.label,
+                    }),
+                  })}
+                >
+                  Send email to {selectedCity.label} officials
+                </ExternalLink>
+              </Box>
+            </Box>
+          )}
+
+          <Box mt={5} pt={3} borderTop={`1px solid ${colors.textGray}`}>
+            <Text.Body color="white" mb={2}>
+              Speak up, Speak now is an intitative that empowers the people to
+              hold their city officials accountable. Thanks to the help of a LOT
+              of people, we've collected the contact information of city
+              officials from all over California, and even several major U.S
+              cities. Want to contribute? Reach out at{' '}
+              <ExternalLink href="mailto:tips@speakupspeaknow.org">
+                tips@speakupspeaknow.org
+              </ExternalLink>
+              .
+            </Text.Body>
+
+            <Text.Body color="white">
+              If your city doesn't have the contact info of your city officials
+              inputted yet, please send an email to{' '}
+              <ExternalLink href="mailto:tips@speakupspeaknow.org">
+                tips@speakupspeaknow.org
+              </ExternalLink>
+              .
+            </Text.Body>
+          </Box>
         </Box>
       </Box>
     </Box>
