827bfe27c0b6176429b5ace2648eb98aed3a1781
Monil Patel <monilpat@gmail.com>
2020-06-09 00:45:25 -0700

    Revert "Added New City Request Form"
    This reverts commit bc60fdbd1861c4e43b8790ba59010f2818510187.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -17,7 +17,6 @@ import {
 } from 'src/styles/components/buttonlike'
 import { FacebookShareButton, TwitterShareButton } from 'react-share'
 import { useAnalytics } from 'use-analytics'
-import { ReactTypeformEmbed } from 'react-typeform-embed'
 
 interface CityOption {
   value: string
@@ -34,10 +33,6 @@ const LandingPage = () => {
     null,
   )
   const [personName, setPersonName] = React.useState<string>('')
-
-  const [showNewCityRequestForm, setShowNewCityRequestForm] = React.useState<
-    boolean
-  >(false)
 
   const handleCityChange = React.useCallback(
     (selectedOption: any) => {
@@ -58,7 +53,7 @@ const LandingPage = () => {
   const emails =
     selectedCity !== null ? getEmailsForCity(selectedCity.value) : []
 
-  return !showNewCityRequestForm ? (
+  return (
     <Box
       minHeight="100vh"
       width="100%"
@@ -237,15 +232,15 @@ const LandingPage = () => {
               If your city doesn't have the contact info of your city officials
               inputted yet, please send an email to{' '}
               <ExternalLink
+                href="mailto:tips@speakupspeaknow.org"
                 onClick={() => {
-                  setShowNewCityRequestForm(true)
                   track('Wants New City Added', {
                     name: personName,
                     city: selectedCity,
                   })
                 }}
               >
-                here
+                tips@speakupspeaknow.org
               </ExternalLink>
               .
             </Text.Body>
@@ -254,18 +249,6 @@ const LandingPage = () => {
       </Box>
       <Wave />
     </Box>
-  ) : (
-    <ReactTypeformEmbed
-      popup
-      autoOpen={showNewCityRequestForm}
-      hideHeaders
-      hideFooter
-      buttonText="Go!"
-      url="https://monilpat.typeform.com/to/PnG4ON"
-      onSubmit={() => {
-        setShowNewCityRequestForm(false)
-      }}
-    />
   )
 }
 
--- src/types/untyped-node-modules.d.ts
+++ src/types/untyped-node-modules.d.ts
@@ -1,4 +1,3 @@
 declare module 'css.gg'
 declare module 'use-analytics'
 declare module '@analytics/google-analytics'
-declare module 'react-typeform-embed'
